34dcb3d579ed19eae562903a34af769a55313c3b
tominagamaya <maya.tominaga.9@gmail.com>
2022-10-20 11:45:30 +0900

    add material-ui

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import { Link } from "react-router-dom";
+import Button from '@mui/material/Button';
+import '../App.css';
 
 const Home = () => {
   return (
-    <>
-      <h1>Todo List</h1>
-      <button type="button">
-        <Link to="/todolist">クリックしてください</Link>
-      </button>
-    </>
+    <div className="home">
+      <div className="home-content">
+        <h1 className="red">Todo List</h1>
+        <Button variant="contained" color="success">
+          <Link to="/todolist" className="button-link">クリックしてください</Link>
+        </Button>
+      </div>
+    </div>
   )
 }
 export default Home;
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,43 +1,54 @@
 import React, { useState } from 'react';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper } from '@mui/material';
+import FormLabel from '@mui/material/FormLabel';
+import '../App.css';
 
 const TodoList = () => {
   const [task, setTask] = useState("");
 
   return (
-    <>
-      <div>Todo List</div>
-      <form>
+    <div className="todo">
+      <h1>Todo List</h1>
+      <form className="todo-input-form">
         <div>
-          <label>タスク名</label>
-          <input id="todo" />
+          <FormLabel sx={{ fontSize: 14, padding: 2 }}>タスク名</FormLabel>
+          <TextField id="todo" size="small" stype="text" variant="outlined" sx={{ height: 20 }} />
         </div>
         <div>
-          <label>期日</label>
-          <input id="date" />
+          <FormLabel sx={{ fontSize: 14, padding: 2 }}>期日</FormLabel>
+          <TextField id="date" size="small" type="date" variant="outlined" />
         </div>
-        <div>
-          <button id="submit">追加</button>
+        <div className="todo-input-content">
+          <Button id="submit" color="success" variant="contained" sx={{ marginLeft: 3 }}>追加</Button>
         </div>
       </form>
       <div>
-        <table>
-          <tr>
-            <th>タスク名</th>
-            <th>期日</th>
-            <th>ステータス</th>
-            <th></th>
-          </tr>
-          <tr>
-            <td>洗濯をする</td>
-            <td>2022-10-14</td>
-            <td>未着手</td>
-            <td>
-              <button>削除</button>
-            </td>
-          </tr>
-        </table>
+        <TableContainer component={Paper}>
+          <Table sx={{ minWidth: 500 }} aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell align="center">タスク名</TableCell>
+                <TableCell align="center">期日</TableCell>
+                <TableCell align="center">ステータス</TableCell>
+                <TableCell></TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              <TableRow>
+                <TableCell>洗濯をする</TableCell>
+                  <TableCell align="center">2022-10-14</TableCell>
+                  <TableCell align="center">未着手</TableCell>
+                  <TableCell>
+                    <Button variant="outlined" color="error">削除</Button>
+                  </TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+        </TableContainer>
       </div>
-    </>
+    </div>
   );
 };
 export default TodoList;
